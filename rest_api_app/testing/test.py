d0cd9843fcc5ed21fa80147a23a9cb3f0772b798
Manish <manishk19365@gmail.com>
2021-02-11 09:37:55 +0530

    timeout script added

--- rest_api_app/testing/test.py
+++ rest_api_app/testing/test.py
@@ -4,22 +4,18 @@ import json
 
 def create_resource():
     new_data = {
-        'techie_id': 1019,
-        'techie_name': 'hish',
-        'techie_skill': 'Scala',
-        'techie_profile': 'Scala dev',
-        'techie_salary': 100000
+
     }
     try:
-        resp = requests.post('http://127.0.0.1:8000/rest_api/api/', data=json.dumps(new_data),timeout=0.000002)
+        resp = requests.get('http://127.0.0.1:8000/rest_api/api/', data=json.dumps(new_data),
+                            timeout=0.0000000000000002)
         print(resp.status_code)
         print(resp.json())
     except requests.exceptions.ReadTimeout:
-        print('Time out')
+        print('Time out Error')
 
 
 create_resource()
-
 
 # def getresource():
 #     new_data = {
